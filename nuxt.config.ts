7e3b239e8f9eadd5476d0c48580a5ea84e4b6f55
Ian Kennedy <ian@resolutionathens.com>
2025-08-08 09:59:16 -0400

    Replace favicon.ico with camera icon and optimize favicon configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,8 +54,8 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }
       ]
     }
   }
